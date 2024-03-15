e1daf9d0818dfa5af49ba90967f917c057d22dfa
Állyson Philipe Barbosa de Brito <20211044110007@ifrn.local>
2024-03-14 21:33:57 -0300

    segunda aula de laravel

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/bem-vindo', function () {
+    return view('bem-vindo');
+});
+
+Route::get('/hello-world', function () {
+    return view('helloWorld');
+});
+
+// Route::get('/apresentacao/{$id}', function ($id) {
+//     return $id;
+// });
+
